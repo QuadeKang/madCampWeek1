1ce070649ca445be2960279e2ff6b7ee19a4c481
wjeongchoi <artificialmint@gmail.com>
2024-01-01 20:48:16 +0900

    :lipstick: colors.dart

---
+++ src/tabapp/lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundGray = Color(0xFFECECEC);
+  static const Color gray = Color(0xFF979797);
+  static const Color primaryBlue = Color(0xFF476BEC);
+  static const Color icongray = Color(0xFF7C7C7C);
+  static const Color iconred = Color(0xFFD62727);
+  static const Color icongreen = Color(0xFF48C927);
+  static const Color iconskyblue = Color(0xFF57B8FF);
+}
